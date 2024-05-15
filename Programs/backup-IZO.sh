826a0b154620e28abfb2fa0dc561d41694473e12
HandyGold75 <Ian.AnMaZo@gmail.com>
2024-05-15 22:36:32 +0200

    15-May-24

--- Programs/backup-IZO.sh
+++ Programs/backup-IZO.sh
@@ -16,7 +16,7 @@ rm -fv "${backupDir}home.tar.gz"
 find ./ -maxdepth 1 -type f,l | tar -czhvf "${backupDir}home.tar.gz" -T -
 
 for d in $(find ./ -maxdepth 1 -type d -printf "%f\n"); do
-    if [[ "./|./.cache|./.local|./.steam" == *"$d"* ]]; then
+    if [[ "./|./.cache|./.wine|./.local|./.steam" == *"$d"* ]]; then
         continue
     fi
 
