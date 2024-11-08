6657f8307c6de5f2bed5efdca688b44f6a500178
HandyGold75 <Ian.AnMaZo@gmail.com>
2024-11-08 10:53:59 +0100

    08-Nov-24

--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -48,6 +48,8 @@ return {
 			})
 		end,
 		keys = {
+			{ "za", "za", { "n" }, remap = false, silent = true, desc = "Ufo open all folds" },
+			{ "zA", "zA", { "n" }, remap = false, silent = true, desc = "Ufo open all folds" },
 			{ "zR", ":lua require('ufo').openAllFolds()<CR>", { "n" }, silent = true, desc = "Ufo open all folds" },
 			{ "zM", ":lua require('ufo').closeAllFolds()<CR>", { "n" }, silent = true, desc = "Ufo close all folds" },
 			{ "zr", ":lua require('ufo').openFoldsExceptKinds()<CR>", { "n" }, silent = true, desc = "Ufo open all folds" },
