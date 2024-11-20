7e620761ad07458e77e175c789bb82e9e9d5e126
HandyGold75 <Ian.AnMaZo@gmail.com>
2024-11-20 08:51:54 +0100

    20-Nov-24

--- .config/nvim/lua/plugins/spaceport.lua
+++ .config/nvim/lua/plugins/spaceport.lua
@@ -6,7 +6,8 @@ return {
 		config = function()
 			require("spaceport").setup({
 				projectEntry = "NvimTreeOpen",
-				sections = { "_global_remaps", "name", "remaps", "recents", "hacker_news" },
+				sections = { "_global_remaps", "name", "remaps", "recents" },
+				-- sections = { "_global_remaps", "name", "remaps", "recents", "hacker_news" },
 			})
 		end,
 		keys = {
