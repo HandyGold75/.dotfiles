2a81c1e1ed6c6bf72971c22b3216405d205d8a84
HandyGold75 <IZO@HandyGold75.com>
2024-11-22 15:43:39 +0100

    22-Nov-24

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -7,10 +7,10 @@ return {
 			require("conform").setup({
 				notify_on_error = true,
 				notify_no_formatters = true,
-				format_on_save = {
-					timeout_ms = 1000,
-					lsp_fallback = true,
-				},
+				format_on_save = function(bufnr)
+					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
+					return { timeout_ms = 2500, lsp_format = "fallback" }
+				end,
 
 				formatters_by_ft = {
 					lua = { "stylua" },
@@ -42,6 +42,23 @@ return {
 					black = { args = { "--line-length", "256", "--stdin-filename", "$FILENAME", "--quiet", "-" } },
 					gdformat = { args = { "-l", "256", "-" } },
 				},
+			})
+
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true,
+			})
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
 			})
 		end,
 	},
