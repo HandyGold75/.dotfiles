17f824d78b7fc968c89b8acd14a04f7b736627fb
HandyGold75 <Ian.AnMaZo@gmail.com>
2025-01-22 13:46:01 +0100

    22-Jan-25

---
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"iamcco/markdown-preview.nvim",
+		name = "markdown-preview",
+		build = function()
+			vim.cmd(":Lazy load markdown-preview<CR>")
+			vim.fn["mkdp#util#install"]()
+		end,
+		ft = { "markdown" },
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+			vim.g.mkdp_open_to_the_world = 1
+			vim.g.mkdp_open_ip = "127.0.0.1"
+			vim.g.mkdp_port = 5051
+			vim.g.mkdp_echo_preview_url = 1
+			vim.g.mkdp_browser = "none"
+		end,
+	},
+}
