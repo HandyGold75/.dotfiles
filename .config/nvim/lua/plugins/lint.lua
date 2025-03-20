c433a4bcf6bceebf3c8e2a9e123f5d869c37330c
HandyGold75 <IZO@HandyGold75.com>
2025-03-20 19:37:47 +0100

    20-Mar-25

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -7,6 +7,7 @@ return {
 			local lint = require("lint")
 
 			lint.linters_by_ft = {
+				go = { "golangcilint" },
 				python = { "pylint" },
 				sh = { "shellcheck" },
 				bash = { "shellcheck" },
