697ac0ebc600ade1326a11afc30b9f734a2a6560
HandyGold75 <IZO@HandyGold75.com>
2026-02-17 09:03:20 +0100

    17-Feb-26

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -8,7 +8,11 @@ return {
 		ft = { "markdown" },
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
+			vim.g.mkdp_auto_start = 1
+			vim.g.mkdp_auto_close = 0
+			vim.g.mkdp_combine_preview = 1
+			vim.g.updatetime = 100
+			vim.g.mkdp_filetypes = { "markdown", "text" }
 			vim.g.mkdp_open_to_the_world = 1
 			vim.g.mkdp_open_ip = "127.0.0.1"
 			vim.g.mkdp_port = 5051
