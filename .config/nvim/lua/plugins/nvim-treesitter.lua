8d168b365f538e0027fc94b1ec71db32332c5e2d
HandyGold75 <IZO@HandyGold75.com>
2026-06-02 20:14:30 +0200

    02-Jun-26

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,16 +1,26 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPre", "BufNewFile" },
+		lazy = false,
+		build = ":TSUpdate",
+		-- event = { "BufReadPre", "BufNewFile" },
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				parser_install_dir = vim.fn.stdpath("data") .. "/lazy/nvim-treesitter",
-				-- ensure_installed = "all",
-				auto_install = false,
-				highlight = { enable = true, additional_vim_regex_highlighting = false, disable = { "gomod", "markdown" } },
-				indent = { enable = true },
-			})
+			require("nvim-treesitter").setup({ install_dir = vim.fn.stdpath("data") .. "/lazy/nvim-treesitter" })
 
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "<filetype>" },
+				callback = function()
+					-- Highlighting
+					vim.treesitter.start()
+
+					-- Folds
+					vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					vim.wo[0][0].foldmethod = "expr"
+
+					-- Indentation
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
 			vim.api.nvim_create_user_command("TSInstallAll", function() vim.cmd("TSInstall all") end, {})
 		end,
 	},
